a5d9663569e8b6c14fcb73a1bbc9f1082dcffc2e
julyamacedo <julya.macedo@edu.senai.br>
2026-07-30 11:03:47 -0300

    fix:mudar nome do yago

---
+++ revisaoGit.js
@@ -0,0 +1,13 @@
+let equipe = [
+
+    {
+        nome: "julya",
+        cargo: "Desenvolvedora",
+        idade: 17
+    },
+    {
+        nome: "Yago",
+        cargo: "Designer",
+        idade: 16
+    }
+]
